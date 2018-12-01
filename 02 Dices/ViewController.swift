a8418d84db606f25fa2fa08bee9371a039d95a03
Antonio <antoniogamez@agenciaadhoc.com>
2018-12-01 11:33:42 +0100

    Aqui los dos dados funcionando al 100%

--- 02 Dices/ViewController.swift
+++ 02 Dices/ViewController.swift
@@ -10,14 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Propiedades
+    
+    @IBOutlet weak var imageViewDadoIzquierda: UIImageView!
+    @IBOutlet weak var imageViewDadoDerecha: UIImageView!
+    @IBOutlet weak var buttonJugar: UIButton!
+    
+    var valorDado1 = Int.random(in: 1 ..< 7)
+    var valorDado2 = Int.random(in: 1 ..< 7)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Mierda")
+        self.imageViewDadoIzquierda.image = UIImage(named: "dado\(valorDado1)")
+        self.imageViewDadoDerecha.image = UIImage(named: "dado\(valorDado2)")
     }
 
-
-
+    // Métodos
+    @IBAction func jugarPulsado(_ sender: UIButton) {
+        valorDado1 = Int.random(in: 1 ..< 7)
+        valorDado2 = Int.random(in: 1 ..< 7)
+        self.imageViewDadoIzquierda.image = UIImage(named: "dado\(valorDado1)")
+        self.imageViewDadoDerecha.image = UIImage(named: "dado\(valorDado2)")
+    }
+    
     
     
 }
